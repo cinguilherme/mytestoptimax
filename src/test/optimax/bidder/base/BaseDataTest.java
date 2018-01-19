f72d37704fecb86d781b3b2cb51af09c8726e360
cingu <cingu@DESKTOP-HFK9FFT>
2018-01-18 22:19:19 -0300

    add Junit and create baseData test class.

---
+++ src/test/optimax/bidder/base/BaseDataTest.java
@@ -0,0 +1,43 @@
+package test.optimax.bidder.base;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+import optimax.bidder.base.BaseData;
+
+class BaseDataTest {
+
+	BaseData data;
+
+	public BaseDataTest() {
+		data = new BaseData();
+	}
+
+	@Test
+	void testAverageWinning() {
+		Assert.assertTrue(data.averageWinningBid() == 0);
+		
+		data.winingBids.add(10);
+		data.winingBids.add(10);
+		Assert.assertTrue(data.averageWinningBid() == 10);
+		
+		data.winingBids.add(20);
+		data.winingBids.add(20);
+		Assert.assertTrue(data.averageWinningBid() == 60/4);
+	}
+
+	@Test
+	void testAverageBidds() {
+		Assert.assertTrue(data.averageBid() == 0);
+		
+		data.allBids.add(10);
+		data.allBids.add(10);
+		Assert.assertTrue(data.averageBid() == 10);
+		
+		data.allBids.add(20);
+		data.allBids.add(20);
+		Assert.assertTrue(data.averageBid() == 60/4);
+	}
+
+	
+}
