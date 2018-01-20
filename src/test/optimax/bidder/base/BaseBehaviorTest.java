26f3884e2991b4762fc8829eb5e1387cc78365f7
cingu <cingu@DESKTOP-HFK9FFT>
2018-01-20 08:48:55 -0300

    behavior test, tested basic open, repond and bait

---
+++ src/test/optimax/bidder/base/BaseBehaviorTest.java
@@ -0,0 +1,50 @@
+package test.optimax.bidder.base;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+import optimax.bidder.base.BaseBidder;
+import optimax.bidder.behaviourcontrol.Behaviour;
+import optimax.bidder.behaviourcontrol.enums.BehaviorMultiplierEnum;
+
+class BaseBehaviorTest {
+
+	Behaviour behaveior;
+	BaseBidder bidder;
+
+	public BaseBehaviorTest() {
+		behaveior = new Behaviour(BehaviorMultiplierEnum.SHY) {
+		};
+		
+		bidder = new BaseBidder() {
+		};
+		
+	}
+
+	@Test
+	void testBaseOpenner() {
+		
+		Assert.assertTrue(behaveior.opener(bidder) == 0);
+	}
+	
+	@Test
+	void testBaseRespond() {
+		bidder.opponentData.lastBid = 10;
+		
+		Assert.assertTrue(behaveior.respond(bidder) == 12);
+	}
+
+	@Test
+	void testBaseBait() {
+		bidder.opponentData.lastBid = 40;
+		bidder.opponentData.winingBids.add(30);
+		bidder.opponentData.winingBids.add(32);
+		bidder.opponentData.winingBids.add(31);
+		bidder.opponentData.winingBids.add(28);
+		
+		Assert.assertTrue(behaveior.bait(bidder) == 0);
+
+	}
+
+	
+}
