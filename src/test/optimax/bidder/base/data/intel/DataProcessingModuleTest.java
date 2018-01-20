80a658e688229de38b7865015870e0b8c6b71995
cingu <cingu@DESKTOP-HFK9FFT>
2018-01-20 11:48:49 -0300

    creation of test class for the dataprocessing

---
+++ src/test/optimax/bidder/base/data/intel/DataProcessingModuleTest.java
@@ -0,0 +1,13 @@
+package test.optimax.bidder.base.data.intel;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DataProcessingModuleTest {
+
+	public DataProcessingModuleTest() {
+		
+	}
+
+}
