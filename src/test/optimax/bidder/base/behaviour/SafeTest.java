770e1a77c2c9c5ad05670d5b08fad01860d2eee0
cingu <cingu@DESKTOP-HFK9FFT>
2018-01-20 09:06:15 -0300

    create single testable method

--- src/test/optimax/bidder/base/behaviour/SafeTest.java
+++ src/test/optimax/bidder/base/behaviour/SafeTest.java
@@ -1,17 +1,26 @@
 package test.optimax.bidder.base.behaviour;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
 
+import optimax.bidder.base.BaseBidder;
 import optimax.bidder.behaviourcontrol.impl.Safe;
 
 class SafeTest {
 
 	Safe behaviour;
-
+	BaseBidder bidder;
+	
 	public SafeTest() {
 		behaviour = new Safe();
+		bidder = new BaseBidder() {
+		};
 	}
 
+	@Test
+	void testRespond() {
+		
+		
+		behaviour.respond(bidder);
+	}
+	
 }
