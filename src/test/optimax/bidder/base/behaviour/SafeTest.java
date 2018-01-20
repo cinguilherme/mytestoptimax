f1b8e7bdf3889b6e6928f5cc4850778fc50599a8
cingu <cingu@DESKTOP-HFK9FFT>
2018-01-20 09:03:50 -0300

    create Safe test class

---
+++ src/test/optimax/bidder/base/behaviour/SafeTest.java
@@ -0,0 +1,17 @@
+package test.optimax.bidder.base.behaviour;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import optimax.bidder.behaviourcontrol.impl.Safe;
+
+class SafeTest {
+
+	Safe behaviour;
+
+	public SafeTest() {
+		behaviour = new Safe();
+	}
+
+}
