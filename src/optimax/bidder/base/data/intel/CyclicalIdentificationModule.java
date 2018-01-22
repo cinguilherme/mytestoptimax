d2b83240ef6315f7b2dd5507382dccc6d62d97b6
cingu <cingu@DESKTOP-HFK9FFT>
2018-01-22 20:33:58 -0300

    organize imports

--- src/optimax/bidder/base/data/intel/CyclicalIdentificationModule.java
+++ src/optimax/bidder/base/data/intel/CyclicalIdentificationModule.java
@@ -2,7 +2,6 @@ package optimax.bidder.base.data.intel;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import optimax.bidder.base.data.BiddingData;
