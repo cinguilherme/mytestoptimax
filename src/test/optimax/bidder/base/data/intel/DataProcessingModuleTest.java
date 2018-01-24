35ef492a973993fff59db6ade8e9aa3c4265ec8c
cingu <cingu@DESKTOP-HFK9FFT>
2018-01-23 21:13:20 -0300

    test average writen

--- src/test/optimax/bidder/base/data/intel/DataProcessingModuleTest.java
+++ src/test/optimax/bidder/base/data/intel/DataProcessingModuleTest.java
@@ -2,14 +2,26 @@ package test.optimax.bidder.base.data.intel;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
+
+import optimax.bidder.base.data.intel.DataProcessingModule;
 
 class DataProcessingModuleTest {
 
+	List<Integer> bids;
+	
 	public DataProcessingModuleTest() {
-		
+		bids = Arrays.asList(1,2,3,4,5,6);
 	}
 	
-	
+	@Test
+	void testAverage() {
+		Integer average = DataProcessingModule.average(bids);
+		assertTrue(average.equals(3));
+	}
 
 }
